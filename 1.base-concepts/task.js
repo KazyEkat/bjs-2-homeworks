83c6e914ba2b7bf55e2ddc8131d3c17a6f5680aa
Ekatherine Kuznetsova <sadilker@mail.ru>
2024-04-15 11:59:03 +0300

    Реализовано первое задание

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -2,9 +2,22 @@
 function solveEquation(a, b, c) {
   let arr = [];
   
+  let x1;
+  let x2;
+  let D = b ** 2 - 4 * a * c;
+  if (D > 0) {
+    x1 = (-b + Math.sqrt(D))/(2 * a);
+    x2 = (-b - Math.sqrt(D))/(2 * a);
+    arr.push(x1, x2);
+  }
+  else if (D === 0) {
+    x1 = -b / (2 * a);
+    arr.push(x1);
+  }
+
   return arr;
 }
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
+// function calculateTotalMortgage(percent, contribution, amount, countMonths) {
   
-}
+// }
